4bc76993254e54909464f0dab72ce917b508c3c2
ryan <kuyoeku@gmail.com>
2026-09-04 21:38:45 +0800

    fix(lint): put bun:test after node: builtins in web-search tests
    Pre-push eslint failed on import/order, which blocked the push.

--- packages/plugins/src/__tests__/web-search.test.ts
+++ packages/plugins/src/__tests__/web-search.test.ts
@@ -1,8 +1,8 @@
 // Copyright (c) 2026 Juan Ignacio Molina Estrada
 // SPDX-License-Identifier: FSL-1.1-Apache-2.0
-import { describe, expect, test } from 'bun:test'
 import { readFileSync } from 'node:fs'
 import { join } from 'node:path'
+import { describe, expect, test } from 'bun:test'
 import { fanOutWebSearch, readSearchRequest } from '../lib/web-search'
 import { mapTavilyResults } from '../tavily-search'
 import { exaBrowserRefusal, mapExaResults } from '../exa-search'
